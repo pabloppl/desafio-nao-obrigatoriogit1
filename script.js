71a8653388b36dbf84910a5efa2ff17778495a3e
Pablo Pereira <pablopereira444@hotmail.com>
2024-04-12 18:23:58 -0300

    Adicionando mais um valor para as somas

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-valor1 = parseInt(prompt('Insira um valor aqui'))
-valor2 = parseInt(prompt('Insira outro valor'))
+valor1 = parseInt(prompt('Insira um valor aqui'));
+valor2 = parseInt(prompt('Insira outro valor'));
+valor3 = parseInt(prompt('Insira outro valor'));
 
-soma = valor1 + valor2
 
-console.log (`A soma de ${valor1} e ${valor2} é igual a ${soma}`)
+soma = valor1 + valor2 + valor3;
+
+console.log (`A soma de ${valor1} e ${valor2} é igual a ${soma}`);
+
+function calculoImc() {
+    let peso = parseFloat(prompt('Insira o seu peso para calcular o imc'));
+    let altura = parseFloat(prompt('Insira a sua altura'));
+
+    let resultado = peso / (altura * altura);
+    console.log(`O seu imc é igual a ${resultado.toFixed(2)}.`);
+
+}
+
+calculoImc();
