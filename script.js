bb4b5d7db1d1d68bba476754cd64c5f4676bd574
Pablo Pereira <pablopereira444@hotmail.com>
2024-04-12 17:51:33 -0300

    desafio nao obrigadogit1

---
+++ script.js
@@ -0,0 +1,6 @@
+valor1 = parseInt(prompt('Insira um valor aqui'))
+valor2 = parseInt(prompt('Insira outro valor'))
+
+soma = valor1 + valor2
+
+console.log (`A soma de ${valor1} e ${valor2} é igual a ${soma}`)
